f7b945b8b650a22de0c271d9046dc581dc9c928d
quangtrin <Quang@gmail.com>
2023-01-09 02:19:10 +0700

    a

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyles>
-      <BrowserRouter basename="/frontend_web">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </GlobalStyles>
