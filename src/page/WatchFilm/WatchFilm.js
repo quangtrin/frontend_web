a5a6ae58b3bf6999275bf374242486b175dd173b
= <=>
2023-01-20 21:59:47 +0700

    update 21:59 20-01-2023

--- src/page/WatchFilm/WatchFilm.js
+++ src/page/WatchFilm/WatchFilm.js
@@ -59,7 +59,7 @@ const WatchFilm = ({ user, setIsSignUp }) => {
       {isHasData ? (
         <div className={cx("layout")}>
           <div className={cx("layout_video", "container")}>
-            <iframe className={cx("video")} src={urlEpisodeFilm} allowfullscreen></iframe>
+            <iframe title="video" className={cx("video")} src={urlEpisodeFilm} allowFullScreen></iframe>
           </div>
 
           <Navbar
